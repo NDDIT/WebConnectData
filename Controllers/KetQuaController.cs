4d7dc23e40b1784f1d1b4749441c9b31a8b186e4
Nguyen Duy Duong <2124801030182@student.tdmu.edu.vn>
2023-09-03 13:03:05 +0700

    Add project

---
+++ Controllers/KetQuaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConnectToData.Models;
+
+namespace ConnectToData.Controllers
+{
+    public class KetQuaController : Controller
+    {
+        // GET: KetQua
+        DataClasses1DataContext data = new DataClasses1DataContext("Data Source=MSI;Initial Catalog=QL_Sinh_Vien1;Integrated Security=True");
+        public ActionResult Index()
+        {
+            var All_KetQua = from tt in data.KETQUAs select tt;
+            return View(All_KetQua);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConnectToData.MvcApplication" Language="C#" %>
